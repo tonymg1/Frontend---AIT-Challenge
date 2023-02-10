af6abe07bfd0d7454acc86bc6ef763f4148c0d97
tonymg1 <tonymartingaldeano@gmail.com>
2023-02-10 10:01:19 +0100

    select

--- src/gif/components/HeaderHome.jsx
+++ src/gif/components/HeaderHome.jsx
@@ -1,18 +1,9 @@
 import React from 'react'
 import '../ui/headerHome.styles.css'
-import gifhub from '../../assets/img/gitGif.gif'
+
 
 export const HeaderHome = ({gifs}) => {
 
-  // const blankContent = () => {
-  //   return(
-  //     <>
-  //       <h1 className="header-title text-center">Welcome to Gif Hub</h1>
-  //       <p>This is a gifs app where you can search, view and save your own gif collection</p>
-  //       <img src={gifhub} alt="Example gif" />
-  //     </>
-  //   )
-  // }
 
   const showGifs = () => (
     <>
--- src/gif/components/UploadGif.jsx
+++ src/gif/components/UploadGif.jsx
@@ -2,24 +2,26 @@ import React, { useState } from 'react';
 import axios from "axios";
 
 export const UploadGif = () => {
-  const [file, setFile] = useState()
-  const [category, setCategory] = useState("")
+  const [file, setFile] = useState();
+  const [category, setCategory] = useState("");
+
+  const categories = ["sports", "animals", "funny", "nature", "music", "movies"];
 
   const handleFileChange = (e) => {
     const { files } = e.target;
     if (files) {
-      setFile(files[0])
+      setFile(files[0]);
     } else {
-      setFile(undefined)
+      setFile(undefined);
     }
-  }
+  };
 
   const handleOnClick = () => {
     const formData = new FormData();
     formData.append('title', "titi");
     formData.append('category', category);
     formData.append('content', file);
-    console.log(category)
+    console.log(category);
     axios({
       method: "post",
       url: "http://localhost:4000/api/gif",
@@ -28,22 +30,29 @@ export const UploadGif = () => {
         'Accept': 'application/json',
         "Content-Type": "multipart/form-data",
       },
-    }).then(() => alert("Uploaded!"))
-    .catch(() => alert("Something went wrong :("))
-  }
+    })
+      .then(() => alert("Uploaded!"))
+      .catch(() => alert("Something went wrong :("));
+  };
 
-  const handleCategory =(e)=>{
-    setCategory(e.target.value)
-  }
-  
+  const handleCategory = (e) => {
+    setCategory(e.target.value);
+  };
 
   return (
     <header>
       <div>
         <input type="file" onChange={handleFileChange} />
-        <input type="text" onChange={handleCategory} />
+        <select onChange={handleCategory}>
+          <option value="">Select a category</option>
+          {categories.map(cat => (
+            <option key={cat} value={cat}>
+              {cat}
+            </option>
+          ))}
+        </select>
         <button onClick={handleOnClick}>Upload your gifs</button>
       </div>
     </header>
-  )
-}
+  );
+};
