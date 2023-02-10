df6eb0adffa0ca3af3eaaea40ab427dd896bea43
tonymg1 <tonymartingaldeano@gmail.com>
2023-02-10 11:20:00 +0100

    select

--- src/gif/components/UploadGif.jsx
+++ src/gif/components/UploadGif.jsx
@@ -1,49 +1,64 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from "axios";
 
 export const UploadGif = () => {
-  const [file, setFile] = useState()
-  const [category, setCategory] = useState("")
+  const [file, setFile] = useState();
+  const [categories, setCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/api/gif/categories")
+      .then(({ data }) => {
+        setCategories(data);
+      });
+  }, []);
 
   const handleFileChange = (e) => {
     const { files } = e.target;
     if (files) {
-      setFile(files[0])
+      setFile(files[0]);
     } else {
-      setFile(undefined)
+      setFile(undefined);
     }
-  }
+  };
 
   const handleOnClick = () => {
     const formData = new FormData();
-    formData.append('title', "titi");
-    formData.append('category', category);
-    formData.append('content', file);
-    console.log(category)
+    formData.append("title", "titi");
+    formData.append("category", selectedCategory);
+    formData.append("content", file);
+    console.log(selectedCategory);
     axios({
       method: "post",
       url: "http://localhost:4000/api/gif",
       data: formData,
       headers: {
-        'Accept': 'application/json',
+        Accept: "application/json",
         "Content-Type": "multipart/form-data",
       },
-    }).then(() => alert("Uploaded!"))
-    .catch(() => alert("Something went wrong :("))
-  }
+    })
+      .then(() => alert("Uploaded!"))
+      .catch(() => alert("Something went wrong :("));
+  };
 
-  const handleCategory =(e)=>{
-    setCategory(e.target.value)
-  }
-  
+  const handleCategoryChange = (e) => {
+    setSelectedCategory(e.target.value);
+  };
 
   return (
     <header>
       <div>
         <input type="file" onChange={handleFileChange} />
-        <input type="text" onChange={handleCategory} />
-        <button onClick={handleOnClick}>Upload your gifs</button>
+        <select onChange={handleCategoryChange}>
+          {categories.map((category) => (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
+        <button onClick={handleOnClick}>Upload your gif</button>
       </div>
     </header>
-  )
-}
+  );
+};
