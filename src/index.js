6a7b356adc78a19c4eb8e0e0ef92d9a8eb26e064
tonymg1 <tonymartingaldeano@gmail.com>
2023-02-11 20:12:23 +0100

    wip

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ import { Auth0Provider } from "@auth0/auth0-react"
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
-<Auth0Provider domain='dev-a76jgw4b0myhf3cm.us.auth0.com' clientId='gSRJaskYlhc8PjZ3CPtO0GU6W6Duk8yj' redirectUri={window.location.origin}>
+<Auth0Provider domain='dev-a76jgw4b0myhf3cm.us.auth0.com' clientId='gSRJaskYlhc8PjZ3CPtO0GU6W6Duk8yj' authorizationParams={{
+      redirect_uri: window.location.origin
+    }}>
 <BrowserRouter>
        <App />
 
