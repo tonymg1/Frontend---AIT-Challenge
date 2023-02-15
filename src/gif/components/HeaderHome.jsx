8ab87f7f358ffe87007ddc7c726d251e0a035567
tonymg1 <tonymartingaldeano@gmail.com>
2023-02-15 12:05:52 +0100

    update title

--- src/gif/components/HeaderHome.jsx
+++ src/gif/components/HeaderHome.jsx
@@ -1,9 +1,12 @@
+import { useState } from 'react';
 import axios from 'axios'
 import '../ui/headerHome.styles.css'
 import { ToastContainer, toast } from 'react-toastify';
 import { FaTrash } from 'react-icons/fa';
+
+
 export const HeaderHome = ({gifs}) => {
- 
+  const [postTtile, setPostTitle] = useState([])
 
   const showGifs = () => (
     <>
@@ -11,14 +14,11 @@ export const HeaderHome = ({gifs}) => {
     </>
   )
 
-
-
   const showGif = ({content, title, _id}) => {
     const {data} = content
     const imgSrc = generateSrc(data)
-  
+
     const handleDelete = (id)=>{
-    
       axios.delete(`http://localhost:4000/api/gif/${id}`)
       .then(res => {
         if (res.status === 200) {
@@ -36,8 +36,22 @@ export const HeaderHome = ({gifs}) => {
           draggable: true,
         });
       })
-        
     }
+
+    const handleEdit = (id, currentTitle) => {
+      const updatedTitle = window.prompt('Enter the new title', currentTitle);
+      if (updatedTitle) {
+        axios.patch(`http://localhost:4000/api/gif/edit/${id}`, { title: updatedTitle })
+          .then(response => {
+            const updatedPost = response.data.data;
+            window.location.reload(true);
+            setPostTitle(updatedPost.title);
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      }
+    };
 
     return(
       <>
@@ -45,17 +59,15 @@ export const HeaderHome = ({gifs}) => {
       <img src={imgSrc} className="images"/>
       <p className="image-title">{title}</p>
       <button onClick={()=>handleDelete(_id)}> <FaTrash /></button>
-     
+      <button onClick={()=>handleEdit(_id, title)}>Edit</button>
       </div>
       </>
-      
     )
   }
 
   const noResult = () => {
     return (
       <p>No results</p>
-     
     )
   }
 
@@ -66,7 +78,6 @@ export const HeaderHome = ({gifs}) => {
     return window.btoa(binary);
 };
 
-  // data.img.data.data
   const generateSrc = (data) => {
     const base64Flag = 'data:image/gif;base64,';
     const gifStr = arrayBufferToBase64(data);
@@ -75,10 +86,10 @@ export const HeaderHome = ({gifs}) => {
   }
 
   return (
-<div className="header-home-container">
-  {gifs === undefined }
-  {gifs?.length === 0 && noResult()}
-  {gifs?.length > 0 && showGifs()}
-</div>
+    <div className="header-home-container">
+      {gifs === undefined }
+      {gifs?.length === 0 && noResult()}
+      {gifs?.length > 0 && showGifs()}
+    </div>
   )
 }
